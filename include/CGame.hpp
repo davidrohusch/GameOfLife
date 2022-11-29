14a47df55da4e79cac59ce5e39e461ac533dd83b
david <david.rohusch@post.cz>
2022-11-29 21:39:50 +0100

    Speed controlling

--- include/CGame.hpp
+++ include/CGame.hpp
@@ -2,25 +2,32 @@
 #include "CCamera.hpp"
 #include "CGameboard.hpp"
 #include <SFML/Graphics.hpp>
+#include <SFML/Graphics/Color.hpp>
 #include <cstddef>
 #include <iostream>
 #include <memory>
+#include <optional>
 
 class Game {
 private:
   std::unique_ptr<sf::RenderWindow> window;
-  int speed = 60;
   sf::Clock clock;
-  size_t gameSpeed = 60;
+  size_t frameRate = 60;
+  size_t tickSpeed = 5;
+  const size_t tickSpeedMAX = 10;
+  const size_t tickSpeedMIN = 1;
   sf::Event eventBuffer;
   Camera camera{0, 0, 0};
   GameBoard gameboard;
+  sf::Color backgroundColor{255, 255, 255, 255};
 
   void handleEvents();
   void beginStep();
   void Step();
   void endStep();
   void handleKeyboardInputs();
+
+  void debug_print(std::optional<std::string_view> text) const;
 
 public:
   Game(int height, int width);
--- src/CGame.cpp
+++ src/CGame.cpp
@@ -2,24 +2,52 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Window/Event.hpp>
 #include <bits/iterator_concepts.h>
+#include <cstddef>
 #include <fmt/core.h>
 #include <iostream>
 #include <memory>
+#include <optional>
+#include <string>
 
 Game::Game(int height, int width) {
   window = std::make_unique<sf::RenderWindow>(sf::VideoMode(width, height),
                                               "Game of Life");
-  window->setFramerateLimit(gameSpeed);
+  window->setFramerateLimit(frameRate);
+  window->setKeyRepeatEnabled(false);
 };
 
 Game::~Game(){
 
 };
 
+void Game::debug_print(std::optional<std::string_view> text = std::nullopt) const {
+  if (text.has_value()){
+    fmt::print("{}\n", text.value());
+  }
+}
+
 void Game::handleEvents() {
 
   while (window->pollEvent(eventBuffer)) {
 
+    if (eventBuffer.type == sf::Event::KeyPressed) {
+      if (eventBuffer.key.code == sf::Keyboard::Right) {
+        fmt::print("Speed up\n");
+        tickSpeed = std::min(tickSpeedMAX, tickSpeed + 1);
+      }
+      if (eventBuffer.key.code == sf::Keyboard::Left) {
+        fmt::print("Speed down\n");
+        tickSpeed = std::max(tickSpeedMIN, tickSpeed - 1);
+      }
+      if (eventBuffer.key.code == sf::Keyboard::Escape) {
+        window->close();
+      }
+
+      if (eventBuffer.key.code == sf::Keyboard::D) {
+        debug_print(std::to_string(tickSpeed));
+        debug_print();
+      }
+    }
     if (eventBuffer.type == sf::Event::Closed)
       window->close();
   }
@@ -30,19 +58,16 @@ void Game::endStep() {
   clock.restart();
 }
 
-void Game::handleKeyboardInputs() {
-  if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
-    fmt::print("Speed up\n");
-  }
-  if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left)){
-    fmt::print("Speed up\n");
-  }
-
+[[maybe_unused]] void Game::handleKeyboardInputs() {
+  // TODO: zoom in zoom out with cooldown
+  // if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
+  //   fmt::print("xD\n");
+  // }
 }
 
 void Game::beginStep() {
-  window->clear();
-
+  window->clear(backgroundColor);
+  handleEvents();
 }
 
 void Game::Step() { handleKeyboardInputs(); }
@@ -50,12 +75,10 @@ void Game::Step() { handleKeyboardInputs(); }
 void Game::run() {
 
   while (window->isOpen()) {
-    if (clock.getElapsedTime().asSeconds() >= 1.0f / speed) {
+    if (clock.getElapsedTime().asSeconds() >= 1.0f / frameRate) {
       beginStep();
       Step();
       endStep();
-    } else {
-      handleEvents();
     }
   }
 }
